e33ed3bc5e9e1a1d19f02b25fa72468405f4e847
Guilhermo Masid <guilhermomasid@gmail.com>
2020-10-30 15:03:48 -0300

    bubble sort optimized

---
+++ Bubble Sort/bubbleSortOptimized.js
@@ -0,0 +1,26 @@
+function swap(array, index1, index2) {
+  let temp = array[index1];
+  array[index1] = array[index2];
+  array[index2] = temp;
+}
+
+function sort(array) {
+  let noSwaps;
+
+  for(let i = array.length; i > 0; i--) {
+    noSwaps=true
+    for(let j = 0; j < i - 1; j++){
+      if(array[j] > array[j + 1]) {
+        swap(array, j, j + 1)
+        noSwaps=false;
+      }
+
+    }
+    if(noSwaps) break;
+  } 
+
+  return array;
+}
+
+
+console.log(sort([9, 3, 2, -3, 5, 4, 6, 8, 7]))
