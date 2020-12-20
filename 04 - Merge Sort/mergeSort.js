2ba1e255a2e8adf869652bf0731020b56077bbd8
Guilhermo Masid <guilhermomasid@gmail.com>
2020-12-19 22:37:38 -0300

    new sort algorithm: Merge Sort

--- 04 - Merge Sort/mergeSort.js
+++ 04 - Merge Sort/mergeSort.js
@@ -27,4 +27,12 @@ function merge(array1, array2) {
   return result
 }
 
-console.log(merge([1, 10, 50], [2, 14, 99, 100]))
+function mergeSort(array) {
+  if(array.length <= 1) return array
+  let middle = Math.floor(array.length / 2)
+  let left = mergeSort(array.slice(0, middle))
+  let right = mergeSort(array.slice(middle))
+  return merge(left, right)
+}
+
+console.log(mergeSort([4, 6, 1, 7, 3, 9, 12, 67, 34, 87]))
